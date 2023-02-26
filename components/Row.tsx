d2df5a08dfdaf334d95922a3fc47159496c5499f
Oscar <oscargm28@hotmail.com>
2023-02-26 11:01:30 +0100

    day one finished

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,4 +1,5 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
+import { useRef, useState } from "react";
 import { Movie } from "../interfaces/typings";
 import Thumbnail from "./Thumbnail";
 
@@ -7,6 +8,25 @@ interface Props {
   movies: Movie[];
 }
 const Row = ({ movies, title }: Props) => {
+  // useRef is a quite performance react Hook when is compared to useState.It needs the reference to the stuff referenced
+  const rowRef = useRef<HTMLDivElement>(null);
+  const [isMoved, setIsMoved] = useState(false);
+
+  const handleClick = (direction: string) => {
+    setIsMoved(true);
+    if (rowRef.current) {
+      // si ya se creó la ref al div, miramos cuanto se ha echo scroll desde la izquierda y su ancho total
+      const { scrollLeft, clientWidth } = rowRef.current;
+      // segun la dirección restamos el scrollLeft del ancho total o lo sumamos
+      const scrollTo = direction === "left" ? scrollLeft - clientWidth : scrollLeft + clientWidth;
+      // la clave de todo es el metodo scrollTo({}:options),método al que tiene acceso el div
+      rowRef.current.scrollTo({
+        left: scrollTo,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
       <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
@@ -14,13 +34,25 @@ const Row = ({ movies, title }: Props) => {
       </h2>
       {/* al usar un group-hover el padre debe llevar group ?? */}
       <div className="group relative md:-ml-2">
-        <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
-        <div className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2">
+        <ChevronLeftIcon
+          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
+            !isMoved && "hidden"
+          }`}
+          onClick={() => handleClick("left")}
+        />
+
+        <div
+          ref={rowRef}
+          className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2"
+        >
           {movies.map((movie) => (
-            <Thumbnail key={movie.id} movie={movie}/>
+            <Thumbnail key={movie.id} movie={movie} />
           ))}
         </div>
-        <ChevronRightIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
+        <ChevronRightIcon
+          className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`}
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
